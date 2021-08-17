325afb9e21758ad81fe52b0f279db25a55fd2c9e
Benjamin <ben88200@gmail.com>
2021-08-17 13:17:46 +0200

    Ajout routes GET /:id et POST

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const db = require('../config/database');
-const User = require('../models/User');
+const userController = require('../controllers/userController');
 
 /* GET users listing. */
-router.get('/', (req, res) => {
-  User.findAll()
-    .then(users => {
-      console.log(users);
-      res.sendStatus(200);
-    })
-    .catch(err => console.log(err))
-});
+router.get('/', userController.user_listing);
+router.get('/:id', userController.user_get);
+router.post('/', userController.user_post);
 
 module.exports = router;
