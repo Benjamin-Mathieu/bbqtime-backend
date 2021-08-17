c6b9723076a696c14001d2e6be181a1f7b2c2af2
Benjamin <ben88200@gmail.com>
2021-08-17 17:20:13 +0200

    Ajout et suppression de catégorie

--- controllers/categorieController.js
+++ controllers/categorieController.js
@@ -1,14 +1,48 @@
 const Categorie = require('../models/Categorie');
 
+// GET all products
 const categorie_listing = (req, res) => {
     Categorie.findAll()
-    .then(categories => {
-      console.log(categories);
-      res.sendStatus(200);
+      .then(categories => {
+
+        let products = [];
+        categories.forEach(categorie => {
+          products.push(categorie.libelle);
+          console.log(products);
+        });
+        res.status(200).send({ "produits": products });
+
     })
-    .catch(err => console.log(err))
+    .catch(err => console.log(err))  
+}
+
+// POST new product
+const categorie_post = (req, res) => {
+  Categorie.findOrCreate({where: { libelle: req.body.libelle }})
+    .then(new_product => {
+        res.status(201).send({ "message": "Product added" });
+    })
+    .catch(err => {
+        console.log(err);
+        res.sendStatus(500);
+    });
+}
+
+// DELETE one product
+const categorie_delete = (req, res) => {
+  Categorie.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(deleted_product => {
+        res.status(200).send({"message": "Product deleted"})
+    })
+    .catch(err => console.log(err));
 }
 
 module.exports = {
-    categorie_listing
+  categorie_listing,
+  categorie_post,
+  categorie_delete
 }
--- routes/categories.js
+++ routes/categories.js
@@ -4,5 +4,7 @@ const categorieController = require('../controllers/categorieController');
 
 /* GET users listing. */
 router.get('/', categorieController.categorie_listing);
+router.post('/', categorieController.categorie_post);
+router.delete('/:id', categorieController.categorie_delete);
 
 module.exports = router;
