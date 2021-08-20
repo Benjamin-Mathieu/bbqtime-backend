2d7340e629196527fc0f42adece412c1c0c39fe6
Benjamin <ben88200@gmail.com>
2021-08-20 15:53:58 +0200

    Ajout modèle OderPlats

---
+++ models/OrderPlats.js
@@ -0,0 +1,35 @@
+const Sequelize = require('sequelize');
+const db = require('../config/database');
+const Plat = require('../models/Plat');
+const Order = require('../models/Order');
+
+const OrderPlats = db.define('order', {
+    id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    plat_id: {
+        type: Sequelize.INTEGER,
+        references: {         // OrderPlats belongsTo Plat 1:1
+            model: 'plat',
+            key: 'id'
+        }
+    },
+    order_id: {
+        type: Sequelize.INTEGER,
+        references: {         // OrderPlats belongsTo Order 1:1
+            model: 'order',
+            key: 'id'
+        }
+    },
+    quantity: {
+        type: Sequelize.BIGINT
+    }
+});
+
+// OrderPlats.belongsTo(Plat);
+// OrderPlats.belongsTo(Order);
+
+module.exports = OrderPlats;
