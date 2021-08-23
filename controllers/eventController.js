263c004e24a46feff22cc5bfab73b7f6d73082a6
Benjamin <ben88200@gmail.com>
2021-08-23 09:48:03 +0200

    Récupération des events de l'utilisateur connecté

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,13 +1,34 @@
 const Event = require('../models/Event');
 const jwt = require('jsonwebtoken');
 
-const test = (req, res) => {
-  Event.findOne({
-    where: { name: 'Chalet' }, include: 'user'
+// GET all events
+// const event_listing = (req, res) => {
+//   Event.findAll()
+//     .then(events => {
+//       let events_array = [];
+//       events.forEach(event => {
+//         events_array.push(event);
+//       });
+//       res.status(200).send({ "events": events_array });
+//     })
+//     .catch(err => console.log(err))
+// }
+
+const event_listing = (req, res) => {
+
+  // Get user_id
+  const token = req.headers.authorization.split(" ")[1];
+  const decoded_token = jwt.decode(token);
+
+  Event.findAll({
+    where: { user_id: decoded_token.id }
   })
-    .then(findedUser => {
-      console.log(findedUser);
-      res.status(200).send({ "message": findedUser.user });
+    .then(result => {
+      if (result === null) {
+        res.status(400).send({ "message": "No events to show" });
+      } else {
+        res.status(200).send({ "message": result });
+      }
     })
     .catch((err) => {
       console.log("Error while find user : ", err);
@@ -15,35 +36,22 @@ const test = (req, res) => {
     });
 }
 
-// GET all events
-const event_listing = (req, res) => {
-    Event.findAll()
-    .then(events => {
-      let events_array = [];
-        events.forEach(event => {
-          events_array.push(event);
-        });
-        res.status(200).send({ "events": events_array });
-    })
-    .catch(err => console.log(err))
-}
-
 // GET one event
 const event_get = (req, res) => {
   const event_id = req.params.id;
 
   Event.findByPk(event_id)
-      .then(event => {
-          res.status(200).send(
-              {
-                  id: event.id,
-                  user_id: event.user_id,
-                  name: event.name,
-                  password: event.password
-              }
-          )
-      })
-      .catch(err => console.log(err))
+    .then(event => {
+      res.status(200).send(
+        {
+          id: event.id,
+          user_id: event.user_id,
+          name: event.name,
+          password: event.password
+        }
+      )
+    })
+    .catch(err => console.log(err));
 }
 
 // POST new event
@@ -58,7 +66,7 @@ const event_post = (req, res) => {
     password: req.body.password
   })
     .then(new_event => {
-      res.status(201).send({"message" : "Event created"})
+      res.status(201).send({ "message": "Event created" })
     })
     .catch(err => console.log(err));
 }
@@ -71,27 +79,26 @@ const event_put = (req, res) => {
         id: req.params.id
       }
     })
-      .then(updated_event => {
-          res.status(200).send({"message": "Event updated"})
-      })
-      .catch(err => console.log(err));
+    .then(updated_event => {
+      res.status(200).send({ "message": "Event updated" })
+    })
+    .catch(err => console.log(err));
 }
 
 // DELETE one event
 const event_delete = (req, res) => {
   Event.destroy({
-      where: {
-        id: req.params.id
-      }
+    where: {
+      id: req.params.id
+    }
   })
-      .then(deleted_event => {
-          res.status(200).send({"message": "Event deleted"})
-      })
-      .catch(err => console.log(err));
+    .then(deleted_event => {
+      res.status(200).send({ "message": "Event deleted" })
+    })
+    .catch(err => console.log(err));
 }
 
 module.exports = {
-  test,
   event_listing,
   event_get,
   event_post,
--- routes/events.js
+++ routes/events.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const eventController = require('../controllers/eventController');
 const checkAuthMiddleware = require('../middlewares/checkAuthorization');
 
-router.get('/test', eventController.test);
-router.get('/', eventController.event_listing);
+// router.get('/', eventController.event_listing);
+
+router.get('/', checkAuthMiddleware.checkAuth, eventController.event_listing);
 router.get('/:id', checkAuthMiddleware.checkAuth, eventController.event_get);
 router.post('/', checkAuthMiddleware.checkAuth, eventController.event_post);
 router.put('/:id', checkAuthMiddleware.checkAuth, eventController.event_put);
