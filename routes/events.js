232b04bcf4dfeec71f330b9baccb6405a392ac2b
Benjamin <ben88200@gmail.com>
2021-08-30 17:12:31 +0200

    Ajout cors

--- routes/events.js
+++ routes/events.js
@@ -2,10 +2,16 @@ const express = require('express');
 const router = express.Router();
 const eventController = require('../controllers/eventController');
 const checkAuthMiddleware = require('../middlewares/checkAuthorization');
+const cors = require('cors');
 
 // router.get('/', eventController.event_listing);
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 
-router.get('/', checkAuthMiddleware.checkAuth, eventController.event_listing);
+
+router.get('/', cors(corsOptions), checkAuthMiddleware.checkAuth, eventController.event_listing);
 router.get('/:id/:password', checkAuthMiddleware.checkAuth, eventController.event_get);
 router.post('/', checkAuthMiddleware.checkAuth, eventController.event_post);
 router.put('/:id', checkAuthMiddleware.checkAuth, eventController.event_put);
