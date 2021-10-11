756ce45ab6a9430248ad7cb977737cca4dfb1b11
Benjamin <ben88200@gmail.com>
2021-10-11 15:08:35 +0200

    Fix bug doublon mdp évènement

--- middlewares/checkCodeMiddleware.js
+++ middlewares/checkCodeMiddleware.js
@@ -2,12 +2,23 @@ const Event = require('../models/Event');
 
 function checkCode(req, res, next) {
     const password = req.body.password;
-    Event.findAll({ where: { password: password } })
-        .then((resp) => {
-            if (Object.keys(resp).length > 0) {
-                return res.status(401).send({ "message": "Le mot de passe que vous avez saisie est déjà utilisé sur un autre évènement !" });
-            } else {
+    const eventId = req.body.id;
+    Event.findAll({
+        where: { password: password }
+    })
+        .then((events) => {
+
+            if (events.length === 0) {
                 next();
+            } else {
+                events.forEach(event => {
+                    if (event.id == eventId) {
+                        next()
+                    }
+                    else {
+                        return res.status(401).send({ "message": "Le mot de passe que vous avez saisie est déjà utilisé !" });
+                    }
+                });
             }
         })
         .catch(err => { console.log(err) });
