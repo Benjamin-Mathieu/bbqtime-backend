11c790e5af5f5401688db4b1ce293d0124475d70
Benjamin <ben88200@gmail.com>
2021-11-03 11:10:03 +0100

    Utilisation template via Sendinblue

--- services/email.js
+++ services/email.js
@@ -1,7 +1,6 @@
 const axios = require("axios");
 const Event = require("../models/Event");
 const User = require("../models/User");
-const Order = require("../models/Order");
 
 const headers = {
     "Content-type": "application/json",
@@ -12,26 +11,31 @@ const email = {
     sendEmailInvitation: async (mailUser, event_id) => {
         try {
             const event = await Event.findByPk(event_id, { include: { model: User } });
+
             const dateEvent = new Date(event.date);
             const d = dateEvent.getDate();
             const m = dateEvent.getMonth();
             const y = dateEvent.getFullYear();
-
             const date = `${d}/${m}/${y}`;
 
-            await email.sendEmail(mailUser, "Invitation à un évènement !",
-                `<h2>Vous êtes invités à l'évènement ${event.name} de ${event.user.name} ${event.user.firstname} !</h2>
-                <div>
-                    <ul>
-                        <li>Adresse: ${event.address} ${event.zipcode} ${event.city} </li>
-                        <li>Date: ${date}</li>
-                    </ul>
-                    <p>"${process.env.LOGO_PATH}logo1.png"</p>
-                    <img src="${process.env.LOGO_PATH}logo1.png" alt="logo-bbqtime" />
-                    <p>Pour rejoindre l'évènement, ouvrez l'application et rentrez le code suivant: <b>${event.password}</b></p>
-                    <p>ou scanner ce qrcode: <img src="${event.qrcode}"/>
-                </div>
-                `)
+            await axios.post(
+                "https://api.sendinblue.com/v3/smtp/email",
+                {
+                    'templateId': 1,
+                    'to': [{ email: mailUser }],
+                    'params': {
+                        "eventName": event.name,
+                        "userName": event.user.name,
+                        "userFirstname": event.user.firstname,
+                        "eventAddress": event.address,
+                        "eventDate": date,
+                        "eventPassword": event.password,
+                    },
+                },
+                {
+                    headers,
+                }
+            );
         } catch (error) {
             throw error;
         }
@@ -40,24 +44,41 @@ const email = {
     sendEmailPreventAdminAdd: async (mailUser, event_id, new_user, pass_new_user) => {
         try {
             const event = await Event.findByPk(event_id, { include: { model: User } });
+
             if (new_user) {
-                await email.sendEmail(mailUser, `BBQ-Time: Ajout administrateur par ${event.user.name} ${event.user.firstname}`,
-                    `<h2>Vous êtes administrateur sur l'évènement ${event.name} de ${event.user.name} ${event.user.firstname} !</h2>
-                <div>
-                    <p>Veuillez-vous connectez avec ses identifiants : </b></p>
-                    <p>E-mail : ${new_user.email} Mot de passe : ${pass_new_user}</p>
-                </div>
-                <div>
-                    <p>Vous pourrez ensuite accéder à l'évènement et gérer les commandes dans la catégorie "Mes évènements"</b></p>
-                </div>
-                `);
+                await axios.post(
+                    "https://api.sendinblue.com/v3/smtp/email",
+                    {
+                        'templateId': 2,
+                        'to': [{ email: mailUser }],
+                        'params': {
+                            "eventName": event.name,
+                            "userName": event.user.name,
+                            "userFirstname": event.user.firstname,
+                            "newUserMail": new_user.email,
+                            "newUserPassword": pass_new_user
+                        },
+                    },
+                    {
+                        headers,
+                    }
+                );
             } else {
-                await email.sendEmail(mailUser, `BBQ-Time: Ajout administrateur par ${event.user.name} ${event.user.firstname}`,
-                    `<h2>Vous êtes administrateur sur l'évènement ${event.name} de ${event.user.name} ${event.user.firstname} !</h2>
-                <div>
-                    <p>Vous pouvez dès à présent accéder à l'évènement et gérer les commandes dans la catégorie "Mes évènements"</b></p>
-                </div>
-                `);
+                await axios.post(
+                    "https://api.sendinblue.com/v3/smtp/email",
+                    {
+                        'templateId': 3,
+                        'to': [{ email: mailUser }],
+                        'params': {
+                            "eventName": event.name,
+                            "userName": event.user.name,
+                            "userFirstname": event.user.firstname,
+                        },
+                    },
+                    {
+                        headers,
+                    }
+                );
             }
         } catch (error) {
             throw error;
@@ -74,8 +95,19 @@ const email = {
 
             const code = rInt(100000, 999999);
 
-            await email.sendEmail(mailUser, "Demande de réinitialisation du mot de passe",
-                `<div>Veuillez entrer le code suivant dans l'application: <b>${code}</b></div>`);
+            await axios.post(
+                "https://api.sendinblue.com/v3/smtp/email",
+                {
+                    'templateId': 4,
+                    'to': [{ email: mailUser }],
+                    'params': {
+                        "code": code
+                    },
+                },
+                {
+                    headers,
+                }
+            );
 
             return code;
         } catch (error) {
