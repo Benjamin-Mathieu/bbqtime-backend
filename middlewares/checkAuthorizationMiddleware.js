c3ce43b1eb5f0c11b3d4112968d07ce8f589442a
Benjamin <ben88200@gmail.com>
2021-11-05 14:19:56 +0100

    Changement message retour erreur jwt

--- middlewares/checkAuthorizationMiddleware.js
+++ middlewares/checkAuthorizationMiddleware.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 function checkAuth(req, res, next) {
+
     try {
         const token = req.headers.authorization.split(" ")[1]; // Bearer "token"
         const decoded_token = jwt.verify(token, process.env.JWT_KEY);
@@ -8,15 +9,16 @@ function checkAuth(req, res, next) {
         next();
     } catch (err) {
         if (err.name === "TokenExpiredError") {
-            return res.status(401).send({ "message": 'jwt expired' });
+            res.status(401).send({ "message": "Erreur JWT", "type": "expired" });
         }
         if (err.name === "JsonWebTokenError") {
-            return res.status(401).send({ "message": 'jwt malformed' });
+            res.status(401).send({ "message": "Erreur JWT", "type": "malformed" });
         }
         if (err.name === "NotBeforeError") {
-            return res.status(401).send({ "message": 'jwt not active' });
+            res.status(401).send({ "message": "Erreur JWT", "type": "not active" });
         }
     }
+
 }
 
 module.exports = {
