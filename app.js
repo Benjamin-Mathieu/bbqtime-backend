efae4767e944bff7d979c04ce34a6493fddb8d72
Benjamin <ben88200@gmail.com>
2021-11-12 11:33:17 +0100

    Changement serveur express

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const createError = require('http-errors');
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
-// Routes
 const categoriesRouter = require('./routes/categories');
 const ordersRouter = require('./routes/orders');
 const usersRouter = require('./routes/users');
 const eventsRouter = require('./routes/events');
 const platsRouter = require('./routes/plats');
-// const orderPlatsRouter = require('./routes/orderplats');
 
 const app = express();
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
 // Database
 const db = require("./config/database");
@@ -26,32 +26,11 @@ try {
   console.error('Unable to connect to the database', error);
 }
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors());
-
+// Routes
 app.use('/categories', categoriesRouter);
 app.use('/orders', ordersRouter);
 app.use('/users', usersRouter);
 app.use('/events', eventsRouter);
 app.use('/plats', platsRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-});
 
 module.exports = app;
