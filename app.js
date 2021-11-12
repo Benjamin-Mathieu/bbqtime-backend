906897d2cf273921d7a19c46b8a0213eb4354a58
Benjamin <ben88200@gmail.com>
2021-11-12 16:45:08 +0100

    Ajout configuration app.disable('etag') pour fix erreur http 304 (not modified) sur iOS

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
+app.disable('etag');
 
 // Database
 const db = require("./config/database");
